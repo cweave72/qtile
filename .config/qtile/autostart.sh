2e62ecfb35df56ed8e4f742cf8d318334a10ef09
Craig <cdweave@gmail.com>
2026-03-01 13:01:35 -0500

    Added picom version check in autostart.sh for blurring backend support.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 DIR="/usr/bin"
+
 nm-applet &
-picom --config ~/.config/picom/picom.conf &
+
+PICOM_VER=$(picom --version)
+PICOM_OPTS=
+if [[ "$PICOM_VER" == "v9" ]]; then
+    PICOM_OPTS=--experimental-backends
+fi
+picom --config ~/.config/picom/picom.conf $PICOM_OPTS &
